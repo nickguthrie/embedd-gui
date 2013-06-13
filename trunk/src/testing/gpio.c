d6268073d9860223703c5c8c6314c630decd78a8
nickg4317 <nickg4317@7356e7b9-c811-b78f-f7bb-6e93cdb31174>
2013-06-13 20:06:38 +0000

    Successfully wrote a program to toggle the Status led on and off. Also added binaries to version control.

--- trunk/src/testing/gpio.c
+++ trunk/src/testing/gpio.c
@@ -1,25 +1,87 @@
+////////////////////////////////////////////////////////////////////////////////
+/**
+ *  @file gpio.c
+ *  @brief Toggle pg9 LED gpio.
+ *
+ *  Turns the Status LED on for two seconds, off for two seconds, then exits.
+ *
+ *  @title gpio.c
+ *  @author Nicholas Guthrie
+ *  @created 2013-06-13
+ *
+ *  Compile with: gcc gpio.c -o ../../bin/testing/gpio
+ */
+////////////////////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------ :Libraries:
+//                      _    _ _                 _
+//                     | |  (_) |__ _ _ __ _ _ _(_)___ ___
+//                     | |__| | '_ \ '_/ _` | '_| / -_|_-<
+//                     |____|_|_.__/_| \__,_|_| |_\___/__/
+//
+// -----------------------------------------------------------------------------
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#include <unistd.h> //for sleep
 
-int main() {
-
-    char s_0[] = “0″;
-    char s_1[] = “1″;
+//----------------------------------------------------------------------- :Main:
+//                              __  __      _
+//                             |  \/  |__ _(_)_ _
+//                             | |\/| / _` | | ' \
+//                             |_|  |_\__,_|_|_||_|
+//
+// -----------------------------------------------------------------------------
+int main()
+{
+    // Variables
+    char s_0[] = "0";
+    char s_1[] = "1";
     int fd;
+    int num = 0;
 
-//toggle pg9 LED gpio
+    // Open GPIO Pin
+    if( (fd=open("/sys/devices/virtual/misc/sun4i-gpio/pin/pg9", O_RDWR)) < 0 )
+    {
+	printf("ERROR Opening file, Exiting.\n");
+	return 1;
+    }
+    else
+    {
+	printf("Sucessfully opened file.\n");
+    }
 
-    if((fd=open(“/sys/devices/virtual/misc/sun4i-gpio/pin/pg9″,O_RDWR))<0) return(1);
-    else {
+    // Turn LED On
+    if( write(fd, s_1, strlen(s_1)) < 0 )
+    {
+	printf("  Error writing s_1, Exiting\n");
+    }
+    else
+    {
+	printf("  LED ON\n");
+    }
 
-	while(1) {
+    sleep(2);
 
-	    if(write(fd,s_1,strlen(s_1))<0) return(1);
-	    if(write(fd,s_0,strlen(s_0))<0) return(1);
-	};
+    // Turn LED Off
+    if( write(fd, s_0, strlen(s_0)) < 0 )
+    {
+	printf("  Error writing s_0, Exiting\n");
+    }
+    else
+    {
+	printf("  LED OFF\n");
+    }
 
-	if(close(fd)<0) return(1);
+    sleep(2);
 
+    // Close file
+    if( close(fd)<0 )
+    {
+	printf("ERROR closing file, Exiting.\n");
+	return 1;
+    }
+    else
+    {
+	printf("Sucessfully closed file.\n");
     }
 }
