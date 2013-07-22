002458ce66f7f13b8d366d7cd807a81f050c1af2
nickg4317 <nickg4317@7356e7b9-c811-b78f-f7bb-6e93cdb31174>
2013-07-22 21:19:36 +0000

    Working on basic controller demonstration software.

--- src/testing/Burgler/mainwindow.cpp
+++ src/testing/Burgler/mainwindow.cpp
@@ -11,12 +11,21 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     PUSHED = 0;
     ui->setupUi(this);
-    ui->progressBar->setValue(1);
-    ui->progressBar->setRange(-1,101);
-
+ 
+    
+    // Sliders
     /*connect(ui->verticalSlider,SIGNAL(valueChanged(int)),
       ui->progressBar,SLOT(setValue(int))); */
-                                    
+    connect(ui->hslider_01, SIGNAL(valueChanged(int)),
+	    ui->lcd_01, SLOT(display(int)));
+    connect(ui->hslider_02, SIGNAL(valueChanged(int)),
+	    ui->lcd_02, SLOT(display(int)));
+    connect(ui->hslider_03, SIGNAL(valueChanged(int)),
+	    ui->lcd_03, SLOT(display(int)));
+    connect(ui->hslider_04, SIGNAL(valueChanged(int)),
+	    ui->lcd_04, SLOT(display(int)));
+    connect(ui->hslider_05, SIGNAL(valueChanged(int)),
+	    ui->lcd_05, SLOT(display(int)));
 }
 
 MainWindow::~MainWindow()
@@ -26,15 +35,32 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pb_up_pressed()
 {
-    ui->pb_up->setText("Pushed");
+    /*ui->pb_up->setText("Pushed");
     if(PUSHED < 100)
 	PUSHED += INCRAMENT;
     ui->progressBar->setValue(PUSHED);
+    */
 }
 
 void MainWindow::on_pb_down_pressed()
 {
-    if(PUSHED >= INCRAMENT)
+    /*if(PUSHED >= INCRAMENT)
 	PUSHED -= INCRAMENT;
-   ui->progressBar->setValue(PUSHED);
+	ui->progressBar->setValue(PUSHED);*/
 }
+
+/*void MainWindow::on_hslider_01_sliderMoved(int position)
+{
+    ui->lcd_01->setDecMode();
+}
+
+void MainWindow::on_hslider_01_sliderPressed()
+{
+    ui->lcd_01->display(20);
+}
+
+void MainWindow::on_hslider_02_actionTriggered(int action)
+{
+    
+}
+*/
