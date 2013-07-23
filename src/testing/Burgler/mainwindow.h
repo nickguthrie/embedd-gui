9f2ee812b293b521fe609e2ec6b89ad9b187abb8
nickg4317 <nickg4317@7356e7b9-c811-b78f-f7bb-6e93cdb31174>
2013-07-23 21:28:37 +0000

    Finished basic version of channel controller.  All buttons seem to work with basic functionality.

--- src/testing/Burgler/mainwindow.h
+++ src/testing/Burgler/mainwindow.h
@@ -13,31 +13,20 @@ class MainWindow : public QMainWindow
     
 public:
     explicit MainWindow(QWidget *parent = 0);
-    void ModeButton( int num );
     void UpdateLCD();
     ~MainWindow();
     
 private slots:
-    //void on_pb_up_pressed();
-    //void on_pb_down_pressed();
-    //void on_hslider_01_sliderMoved(int position);
-    //void on_hslider_01_sliderPressed();
-    //void on_hslider_02_actionTriggered(int action);
     void on_cbutton_0_clicked();
     void on_cbutton_1_clicked();
     void on_cbutton_2_clicked();
     void on_cbutton_3_clicked();
     void on_cbutton_4_clicked();
-    //void on_pb_up_3_clicked();
     void on_pb_value_off_clicked();
     void on_pb_value_full_clicked();
-    void on_pb_value_decrament_clicked();
-    void on_pb_value_incrament_clicked();
     void on_pb_value_half_clicked();
     void on_channelSlider_valueChanged(int value);
-
     void on_pb_value_incrament_pressed();
-
     void on_pb_value_decrament_pressed();
 
 protected slots:
