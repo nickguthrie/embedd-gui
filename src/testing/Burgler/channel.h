ae0db9b0d237bff07d894a5d4aa910d0a4348932
nickg4317 <nickg4317@7356e7b9-c811-b78f-f7bb-6e93cdb31174>
2013-07-23 18:13:59 +0000

    Added some classes to handle channel management.

---
+++ src/testing/Burgler/channel.h
@@ -0,0 +1,76 @@
+#ifndef CHANNEL_H
+#define CHANNEL_H
+////////////////////////////////////////////////////////////////////////////////
+/**
+ *  @class channel
+ *  @brief Stores information about each channel.
+ *
+ *  DETAILED NOTES
+ */
+////////////////////////////////////////////////////////////////////////////////  
+class channel
+{
+private:
+    //////////////////////////////
+    // Members
+    //////////////////////////////  
+    int min;
+    int max;
+    int value;
+    // string name; /* worth including a name? */
+    
+public:
+    //////////////////////////////
+    // Constructors
+    //////////////////////////////
+    channel();
+    channel( int min_size, int max_size );
+    
+    //////////////////////////////
+    // Accessors
+    //////////////////////////////
+    int get_value () const;
+    
+    //////////////////////////////
+    // Mutators
+    //////////////////////////////
+    void set_value( int size );
+    void set_size( int min_size, int max_size );
+};
+/* //////////////////////////////////////////////////////////////////////////////// */
+/* /\** */
+/*  *  @class channel_holder */
+/*  *  @brief  */
+/*  * */
+/*  *  DETAILED NOTES */
+/*  *\/ */
+/* ////////////////////////////////////////////////////////////////////////////////   */
+/* class channel_holder */
+/* { */
+/* private: */
+/*     ////////////////////////////// */
+/*     // Members */
+/*     //////////////////////////////   */
+/*     int size; */
+/*     int mode; */
+/*     channel * holder; */
+
+/* public: */
+/*     ////////////////////////////// */
+/*     // Constructors */
+/*     ////////////////////////////// */
+/*     channel_holder( int num_channels ); */
+
+/*     ////////////////////////////// */
+/*     // Accessors */
+/*     ////////////////////////////// */
+/*     int get_size () const; */
+/*     int get_mode () const; */
+/*     channel * get_channel ( int num ); */
+    
+/*     ////////////////////////////// */
+/*     // Mutators */
+/*     ////////////////////////////// */
+/* }; */
+
+#endif // CHANNEL_H
