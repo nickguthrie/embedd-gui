2ae32b6aad9a85c8c29324b8e26727c17213f677
nickg4317 <nickg4317@7356e7b9-c811-b78f-f7bb-6e93cdb31174>
2013-07-29 19:26:22 +0000

    Started work on vital vio light controller. Blank initial screen is working.

---
+++ src/vital_vio/vital_vio/mainwindow.cpp
@@ -0,0 +1,95 @@
+////////////////////////////////////////////////////////////////////////////////
+/**
+ *  @file mainwindow.cpp
+ *  @brief 
+ *
+ *  Detailed Description
+ *  
+ *  @title Vital Vio
+ *  @author Nicholas Guthrie
+ *  @web http//nickguthrie.com
+ *  @created July 29, 2013
+ * 
+ */
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+////////////////////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------ :Libraries:
+//                      _    _ _                 _        
+//                     | |  (_) |__ _ _ __ _ _ _(_)___ ___
+//                     | |__| | '_ \ '_/ _` | '_| / -_|_-<
+//                     |____|_|_.__/_| \__,_|_| |_\___/__/
+//                                                        
+// -----------------------------------------------------------------------------
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPushButton>
+//--------------------------------------------------------------------- :Global:
+//                            ___ _     _          _ 
+//                           / __| |___| |__  __ _| |
+//                          | (_ | / _ \ '_ \/ _` | |
+//                           \___|_\___/_.__/\__,_|_|
+//                                                   
+// -----------------------------------------------------------------------------
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    // Set up User Interface
+    ui->setupUi(this);
+    this->DeleteButtonInit();
+
+    //QPushButton *deleteButton = new QPushButton(this);
+}
+////////////////////////////////////////////////////////////////////////////////
+/**
+ *  @brief Window deconstructor.
+ */
+////////////////////////////////////////////////////////////////////////////////
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+////////////////////////////////////////////////////////////////////////////////
+/**
+ *  @brief Initializes the on default "off state" buton.
+ */
+////////////////////////////////////////////////////////////////////////////////
+void MainWindow::DeleteButtonInit()
+{
+    this->deleteButton = new QPushButton(this);
+    this->deleteButton->setMinimumWidth(500);
+    this->deleteButton->setMinimumHeight(300);
+    this->deleteButton->setStyleSheet("background-color: rgb(0, 0, 0);");
+    connect(deleteButton, SIGNAL(clicked()), this, SLOT(DeleteButtonHide()));
+}
+////////////////////////////////////////////////////////////////////////////////
+/**
+ *  @brief Hides the "off state" button.
+ */
+////////////////////////////////////////////////////////////////////////////////
+void MainWindow::DeleteButtonHide()
+{
+    this->deleteButton->setVisible(false);
+    
+}
+
+void MainWindow::on_fooButton_clicked()
+{
+    this->deleteButton->setVisible(true);
+}
---
+++ src/vital_vio/vital_vio/vital_vio.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-07-29T13:25:44
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = vital_vio
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
